148efeb982f3d218e6c86dd8deeb1a3ba15343d5
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-10-09 04:15:17 +0700

    Small refactor

--- CineMatch.UI/ClientApp/src/AppRoutes.js
+++ CineMatch.UI/ClientApp/src/AppRoutes.js
@@ -5,6 +5,10 @@ const AppRoutes = [
         index: true,
         element: <Home/>
     },
+    {
+        path: '/getMovie',
+        element: <Movie/>
+    },
 ];
 
 export default AppRoutes;
--- CineMatch.UI/ClientApp/src/setupProxy.js
+++ CineMatch.UI/ClientApp/src/setupProxy.js
@@ -5,7 +5,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:11518';
 
 const context = [
-    "/weatherforecast",
+    "/api/movie",
 ];
 
 const onError = (err, req, resp, target) => {
