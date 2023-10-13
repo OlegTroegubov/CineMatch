4761a72724d5a5bbdfdbfb5d5cfbec420a9d7f7d
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-10-13 16:13:39 +0700

    Создал компоненты для отображения Фильма

---
+++ CineMatch.UI/ClientApp/src/components/Button.js
@@ -0,0 +1,12 @@
+﻿import styled from 'styled-components';
+
+export const CustomButton = styled.button`
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  margin: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+`;
+
