3039329e2b8164307e5dd8f1b7fc10ac638d0ecc
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-11-11 19:09:44 +0700

    Добавлена ошибка для токена

---
+++ CineMatch.Application/Common/Exceptions/TokenException.cs
@@ -0,0 +1,13 @@
+﻿namespace CineMatch.Application.Common.Exceptions;
+
+public class TokenException : Exception
+{
+    public TokenException()
+    {
+    }
+
+    public TokenException(string message) : base(message)
+    {
+
+    }
+}
