175d1a1376653b10eda5079b699de9f955425af0
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-11-11 19:09:25 +0700

    Добавлена ошибка наличия

---
+++ CineMatch.Application/Common/Exceptions/ExistsException.cs
@@ -0,0 +1,13 @@
+﻿namespace CineMatch.Application.Common.Exceptions;
+
+public class ExistsException : Exception
+{
+    public ExistsException()
+    {
+    }
+
+    public ExistsException(string message) : base(message)
+    {
+
+    }
+}
