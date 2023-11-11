1544477d6a219edad7565d91fd75d6533c83e0f2
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-11-11 19:15:32 +0700

    Добавлен компонент для регистрации

---
+++ CineMatch.UI/ClientApp/src/components/Registration.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Registration = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister = async () => {
+        try {
+            const requestData = {
+                username: username,
+                password: password
+            };
+
+            await axios.post('/api/user/registration', requestData, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            console.log('Registration successful');
+        } catch (error) {
+            console.error('Registration failed', error);
+        }
+    };
+
+
+    return (
+        <div>
+            <h2>Registration</h2>
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleRegister}>Register</button>
+        </div>
+    );
+};
+
+export default Registration;
