60995d78b21ce38bedc4fdca1aacf780fc428266
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-11-11 19:12:46 +0700

    Добавлен сервис для Jwt

--- CineMatch.UI/Program.cs
+++ CineMatch.UI/Program.cs
@@ -1,13 +1,25 @@
+using System.Text;
 using CineMatch.Application;
 using CineMatch.Infrastructure;
 using CineMatch.UI;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddWebUI();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-
+builder.Services.AddAuthentication().AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            builder.Configuration.GetSection("AppSettings:Token").Value!))
+    };
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +31,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
+app.UseAuthentication(); 
+
+app.UseAuthorization();  
 
 app.MapControllerRoute(
     "default",
