623bd42681f392ff46ee831c428899dd9f727b1b
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-11-11 19:15:48 +0700

    Добавлен компонент для логина

---
+++ CineMatch.UI/ClientApp/src/components/Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('/api/user/login', { username, password });
+            console.log('Login successful. Token:', response.data);
+        } catch (error) {
+            console.error('Login failed', error);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    );
+};
+
+export default Login;
