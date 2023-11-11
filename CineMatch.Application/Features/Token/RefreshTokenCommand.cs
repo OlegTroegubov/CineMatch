ae39efea46636b8fdd8a6acbe6305b9f032b725c
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-11-11 19:08:41 +0700

    Создан хэндлер для обновления токена

---
+++ CineMatch.Application/Features/Token/RefreshTokenCommand.cs
@@ -0,0 +1,25 @@
+﻿using CineMatch.Application.Common.Exceptions;
+using CineMatch.Application.Common.Interfaces;
+using MediatR;
+
+namespace CineMatch.Application.Features.Token;
+
+public record RefreshTokenCommand : IRequest<string>;
+
+public class RefreshTokenCommandHandler : IRequestHandler<RefreshTokenCommand, string>
+{
+    private readonly ITokenService _tokenService;
+
+    public RefreshTokenCommandHandler(ITokenService tokenService)
+    {
+        _tokenService = tokenService;
+    }
+
+    public async Task<string> Handle(RefreshTokenCommand request, CancellationToken cancellationToken)
+    {
+        var newAccessToken = await _tokenService.RefreshToken();
+        return newAccessToken;
+    }
+}
+
+
