742c171892618bf02c5d38943330b4a175f26353
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-11-24 16:01:34 +0700

    Изменена логика выхода

--- CineMatch.Application/Features/User/Commands/LogoutUserCommand.cs
+++ CineMatch.Application/Features/User/Commands/LogoutUserCommand.cs
@@ -1,33 +1,40 @@
 ﻿using CineMatch.Application.Common.Exceptions.User;
 using CineMatch.Application.Common.Interfaces;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 
 namespace CineMatch.Application.Features.User.Commands;
 
-public record LogoutUserCommand(string Username) : IRequest;
+public record LogoutUserCommand : IRequest;
 
 public class LogoutUserCommandHandler : IRequestHandler<LogoutUserCommand, Unit>
 {
     private readonly IApplicationDbContext _context;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public LogoutUserCommandHandler(IApplicationDbContext context)
+    public LogoutUserCommandHandler(IApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
     {
         _context = context;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     public async Task<Unit> Handle(LogoutUserCommand request, CancellationToken cancellationToken)
     {
-        var user = await _context.Users.Include(user => user.RefreshToken)
-            .FirstOrDefaultAsync(user => user.Username == request.Username, cancellationToken);
-        
-        if (user == null)
+        if (_httpContextAccessor.HttpContext != null)
         {
-            throw new NotFoundException("Пользователь не найден");
+            var token = _httpContextAccessor.HttpContext.Request.Cookies["refreshToken"];
+            var user = await _context.Users.Include(user => user.RefreshToken)
+                .FirstOrDefaultAsync(user => user.RefreshToken.Token == token, cancellationToken);
+        
+            if (user == null)
+            {
+                throw new NotFoundException("Пользователь не найден");
+            }
+            user.RefreshToken = null;
+            _httpContextAccessor.HttpContext.Response.Cookies.Delete("refreshToken");
+            await _context.SaveChangesAsync(cancellationToken);
         }
-        user.RefreshToken = null;
-
-        await _context.SaveChangesAsync(cancellationToken);
         return Unit.Value;
     }
 }
