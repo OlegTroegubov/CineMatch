491ec60f8c14791cfeda600e12e9d9c5bdf0d059
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-11-24 23:37:20 +0700

    Пофиксил логику добавления уже существующих жанров в базу

--- CineMatch.Application/Features/User/Commands/LikeMovieCommand.cs
+++ CineMatch.Application/Features/User/Commands/LikeMovieCommand.cs
@@ -21,24 +21,45 @@ public class LikeMovieCommandHandler : IRequestHandler<LikeMovieCommand, Unit>
 
     public async Task<Unit> Handle(LikeMovieCommand request, CancellationToken cancellationToken)
     {
-        var movie = await _context.Movies.AddAsync(new Domain.Entities.Movie
+        var movie = await _context.Movies.FirstOrDefaultAsync(m => m.Id == request.Id, cancellationToken);
+
+        if (movie == null)
         {
-            Id = request.Id,
-            Title = request.Title,
-            Rating = request.Rating,
-            Description = request.Description,
-            ReleaseYear = request.ReleaseYear,
-            PosterUrl = request.PosterUrl,
-            Genres = GenreMapper.MapToListGenre(request.Genres)
-        }, cancellationToken);
+            movie = new Domain.Entities.Movie
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Rating = request.Rating,
+                Description = request.Description,
+                ReleaseYear = request.ReleaseYear,
+                PosterUrl = request.PosterUrl,
+                Genres = new List<Domain.Entities.Genre>()
+            };
+
+            foreach (var genreDto in request.Genres)
+            {
+                var existingGenre = await _context.Genres.FirstOrDefaultAsync(genre => genre.Title == genreDto.Title, cancellationToken);
+
+                if (existingGenre != null)
+                {
+                    movie.Genres.Add(existingGenre);
+                }
+                else
+                {
+                    var newGenre = new Domain.Entities.Genre { Title = genreDto.Title };
+                    movie.Genres.Add(newGenre);
+                }
+            }
+            
+            await _context.Movies.AddAsync(movie, cancellationToken);
+        }
 
         if (_httpContextAccessor.HttpContext != null)
         {
             var username = _httpContextAccessor.HttpContext.User.Claims.FirstOrDefault()?.Value;
-            var user = await _context.Users.Include(user => user.LikedMovies)
-                .FirstOrDefaultAsync(user => user.Username == username, cancellationToken);
-            
-            user.LikedMovies.Add(movie.Entity);
+            var user = await _context.Users.Include(u => u.LikedMovies)
+                .FirstOrDefaultAsync(u => u.Username == username, cancellationToken);
+            user.LikedMovies.Add(movie);
         }
 
         await _context.SaveChangesAsync(cancellationToken);
