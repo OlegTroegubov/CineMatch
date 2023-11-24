085f000ce1c4a4f6abff8b9e080b578e7f58fa5a
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-11-24 16:02:13 +0700

    Создан маппер для жанров

---
+++ CineMatch.Application/Features/Genre/GenreMapper.cs
@@ -0,0 +1,9 @@
+﻿namespace CineMatch.Application.Features.Genre;
+
+public class GenreMapper
+{
+    public static List<Domain.Entities.Genre> MapToListGenre(List<GenreDto> genreDtos)
+    {
+        return genreDtos.Select(genreDto => new Domain.Entities.Genre { Title = genreDto.Title }).ToList();
+    }
+}
